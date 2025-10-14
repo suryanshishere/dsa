6879091fb9bbac6f6788b05dee32d059d637165f
suryanshishere <heresuryanshsingh@gmail.com>
2025-10-14 10:03:03 +0530

    BST working using recursion approach

--- heap/Heap.java
+++ heap/Heap.java
@@ -39,4 +39,39 @@ public class Heap {
         }
     }
 
+    public Integer remove() {
+        if (heap.size() == 0) {
+            return null;
+        }
+        if (heap.size() == 1) {
+            return heap.remove(0);
+        }
+
+        int maxValue = heap.get(0);
+        heap.set(0, heap.remove(heap.size() - 1));
+        sinkDown(0);
+
+        return maxValue;
+    }
+
+    private void sinkDown(int index) {
+        int maxIndex = index;
+        while (true) {
+            int leftIndex = leftChild(index);
+            int rightIndex = rightChild(index);
+            if(leftIndex < heap.size() && heap.get(leftIndex) > heap.get(maxIndex)){
+                maxIndex = leftIndex;
+            }
+            if(rightIndex < heap.size() && heap.get(rightIndex) > heap.get(maxIndex)){
+                maxIndex = rightIndex;
+            }
+            if(index != maxIndex){
+                swap(index, maxIndex);
+                index = maxIndex;
+            }else{
+                return;
+            }
+        }
+    }
+
 }
